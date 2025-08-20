7c783b5a2a02df407680aaa1dd1b012a4a3314d8
ChiranjitDalui <forbca2022@gmail.com>
2025-08-20 20:31:39 +0530

    chore: Remove Day28_RoutingDemo folder

--- Day28_RoutingDemo/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Day_28RoutingDemo.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
